77357a9503e29c8d5b3463c0ec153b6697435018
albert123-ls <59284710+albert123-ls@users.noreply.github.com>
2023-11-20 15:01:32 +0100

    Rajout de l'analyse des paquets

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ import tkinter as tk
 from tkinter import scrolledtext, simpledialog
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.ensemble import IsolationForest
+import subprocess
+import os
+import ctypes
+
 
 class NetworkAnalyzer:
     def _init_(self, interface=None):
         self.interface = interface
         self.packet_counter = pd.DataFrame(columns=['IP', 'Count'])
+        self.anomaly_detector = IsolationForest(contamination=0.05)
         self.blocked_ips = set()
 
     def packet_callback(self, packet):
@@ -59,6 +65,7 @@ class NetworkAnalyzer:
             # Remove old entries
             self.packet_counter = self.packet_counter.groupby(['IP', 'DestinationIP', 'Service', 'Flags', 'Length']).sum().reset_index()
 
+
     def start_sniffer(self):
         scapy.sniff(iface=self.interface, store=False, prn=self.packet_callback)
 
@@ -73,6 +80,9 @@ class NetworkAnalyzer:
             # Visualize traffic
             self.visualize_traffic()
 
+            # Detect anomalies using Isolation Forest
+            self.detect_anomalies()
+
     def visualize_traffic(self):
         # Plot the top 10 IP addresses based on packet count
         top_ips = self.packet_counter.sort_values(by='Count', ascending=False).head(10)
@@ -83,6 +93,28 @@ class NetworkAnalyzer:
         plt.xlabel('IP Address')
         plt.ylabel('Packet Count')
         plt.show()
+
+
+    def detect_anomalies(self):
+        # Prepare data for anomaly detection
+        data = self.packet_counter[['Count']].values.reshape(-1, 1)
+
+        # Fit the Isolation Forest model
+        self.anomaly_detector.fit(data)
+
+        # Predict anomalies
+        predictions = self.anomaly_detector.predict(data)
+
+        # Reset index before filtering anomalies
+        self.packet_counter.reset_index(drop=True, inplace=True)
+
+        # Identify anomalies
+        anomalies = self.packet_counter[self.packet_counter.index.isin(self.packet_counter.index[predictions == -1])]
+
+        if not anomalies.empty:
+            print("\nAnomalies Detected:")
+            print(anomalies)
+
 
     def get_packet_counts(self):
         return self.packet_counter
@@ -101,22 +133,32 @@ class App:
         self.start_button = tk.Button(root, text="Start Sniffer", command=self.start_sniffer)
         self.start_button.pack()
 
+        self.analyze_button = tk.Button(root, text="Analyze Traffic", command=self.analyze_traffic)
+        self.analyze_button.pack()
+
         self.show_counts_button = tk.Button(root, text="Show Packet Counts", command=self.show_packet_counts)
         self.show_counts_button.pack()
+
 
     def start_sniffer(self):
         self.network_analyzer.interface = self.get_network_interface()
         sniffer_thread = threading.Thread(target=self.network_analyzer.start_sniffer)
         sniffer_thread.start()
 
+    def analyze_traffic(self):
+        analysis_thread = threading.Thread(target=self.network_analyzer.analyze_traffic)
+        analysis_thread.start()
+
     def show_packet_counts(self):
         packet_counts = self.network_analyzer.get_packet_counts()
         self.text_area.delete(1.0, tk.END)
         self.text_area.insert(tk.END, packet_counts)
 
+
     def get_network_interface(self):
         interface = simpledialog.askstring("Input", "Enter the network interface (e.g., eth0):")
         return interface
+
 
 if __name__ == "__main__":
     pd.set_option("display.max_rows", None)
